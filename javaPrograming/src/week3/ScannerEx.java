5d2ade474ce8fd3af0ec5ca36f5346647e95f97e
admin <admin@DESKTOP-L5S8T9N>
2024-03-21 13:12:37 +0900

    ff

--- javaPrograming/src/week3/ScannerEx.java
+++ javaPrograming/src/week3/ScannerEx.java
@@ -25,7 +25,7 @@ public class ScannerEx {
 		boolean isSingle = scanner.nextBoolean();
 		System.out.println("독신여주는"+isSingle+"입니다.");
 		
-	 System.out.println("안녕f");
+	 System.out.println("안녕ffff");
 		
 		scanner.close();
 		
