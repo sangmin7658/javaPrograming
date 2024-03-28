dc57b9a24b816ea74ae127b243617f821c2820bc
admin <admin@DESKTOP-L5S8T9N>
2024-03-28 13:08:31 +0900

    12

---
+++ javaPrograming/src/week4/AssignmentlncDecOperator.java
@@ -0,0 +1,26 @@
+package week4;
+
+import javax.annotation.processing.SupportedSourceVersion;
+
+public class AssignmentlncDecOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3,b=3,c=3;
+		
+		a += 3;
+		b *=3;
+		c %= 2;
+		System.out.println("a=" + a +",b="+b+",c="+c);
+		int d=3;
+		a=d++;
+		System.out.println("a=" + a +",d="+d);
+		a=++d;
+		System.out.println("a=" + a +",d="+d);
+		a=d--;
+		System.out.println("a=" + a +",d="+d);
+		a=--d;
+		System.out.println("a=" + a +",d="+d);
+	}
+
+}
---
+++ javaPrograming/src/week4/week4.java
@@ -0,0 +1,25 @@
+package week4;
+
+public class week4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println(69/10);
+//		System.out.println(69%10);
+		
+//		int n 5;
+//		int r = n % 2;
+//		System.out.println(" : r " + r);
+		int i = 1;
+		int j = 5;
+		
+		i=i*3;
+		j=i;
+		
+		
+		System.out.println("i:"+i);
+		System.out.println("j:"+j); 
+		
+	}
+
+}
