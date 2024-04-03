77d2da464a91b1676eb95379ba1226d8e1fede6e
admin <admin@DESKTOP-L5S8T9N>
2024-04-03 11:46:11 +0900

    week5

---
+++ javaPrograming/src/week5/SuccessOrfail.java
@@ -0,0 +1,18 @@
+package week5;
+
+import java.util.Scanner;
+
+public class SuccessOrfail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("점수를 입력하시오:");
+		int score = scanner.nextInt();
+		if(score >= 80)
+			System.out.println("축하합니다! 합격입니다.");
+		scanner.close();
+	}
+
+}
---
+++ javaPrograming/src/week5/weeks5.java
@@ -0,0 +1,16 @@
+package week5;
+
+public class weeks5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=5;
+		
+		if(a<b) {
+		 System.out.println("참입니다.");
+		}
+		else { System.out.println("휴강입니다.");}
+	}
+
+}
