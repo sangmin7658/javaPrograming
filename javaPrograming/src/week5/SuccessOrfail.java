11e149291ead24afb71211344ba6abf1f1fbd2b7
admin <admin@DESKTOP-L5S8T9N>
2024-04-04 12:47:37 +0900

    week5

--- javaPrograming/src/week5/SuccessOrfail.java
+++ javaPrograming/src/week5/SuccessOrfail.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class SuccessOrfail {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("점수를 입력하시오:");
+		System.out.print("점수를 입력하시오:");
 		int score = scanner.nextInt();
-		if(score >= 80)
-			System.out.println("축하합니다! 합격입니다.");
+		if(score >= 80) {
+			System.out.println("축하합니다! 합격입니다.");		
+		}else {
+			System.out.println("아쉽습니다! 불합격입니다.");
+		}
 		scanner.close();
 	}
 
