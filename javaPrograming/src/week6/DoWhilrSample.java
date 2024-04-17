cdb1b2f2129f8fc521086a1e817b262e7eac9c69
admin <admin@DESKTOP-L5S8T9N>
2024-04-17 11:58:11 +0900

    week6

---
+++ javaPrograming/src/week6/DoWhilrSample.java
@@ -0,0 +1,15 @@
+package week6;
+
+public class DoWhilrSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c = 'a';
+		
+		do {
+			System.out.print(c);
+			c = (char)(c + 1);			
+		} while (c <='z');
+	}
+
+}
