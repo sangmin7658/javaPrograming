749de709dbbea8d6d9defb6f908d3a90f5ce6c93
admin <admin@DESKTOP-L5S8T9N>
2024-05-23 12:54:34 +0900

    week10

---
+++ javaPrograming/src/week10/makeArray.java
@@ -0,0 +1,21 @@
+package week10;
+
+public class makeArray {
+
+	static int[] makeArray() {
+		int temp[] = new int[4];
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = i;
+		}
+		return temp;
+	}
+
+	public static void main(String[] args) {
+		int intArray[];
+		intArray = makeArray();
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i] + " ");
+		}
+	}
+
+}
